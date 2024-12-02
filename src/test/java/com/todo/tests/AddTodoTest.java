10435042963cfa782e3ab8870b8ba33318134144
Mrezguiajihen <jihen.mrezguia@gmail.com>
2024-12-02 15:41:00 +0100

    add todo

--- src/test/java/com/todo/tests/AddTodoTest.java
+++ src/test/java/com/todo/tests/AddTodoTest.java
@@ -16,8 +16,13 @@ public class AddTodoTest extends Setup{
 	@Test(priority = 1)
 	public void iCanAddTodo() throws IOException {
 		
-		todoPage = new TodoPage();		
+		todoPage = new TodoPage();
+		boolean inputField = todoPage.isElementDisplayed(TodoPage.inputText);
+		Assert.assertTrue(inputField);
 		todoPage.fillTodo(prop.getProperty("todo1"));
-		
+		String elementTodo = todoPage.checkElementContains(TodoPage.textTodo1);
+		Assert.assertTrue(elementTodo.contains(prop.getProperty("todo1")));
+		boolean checkbox = todoPage.isCheckBoxSelected(TodoPage.checkbox);
+		Assert.assertTrue(checkbox);
 	}
 }
